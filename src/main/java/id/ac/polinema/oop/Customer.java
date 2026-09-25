21f5956b0cf86f264a8bd7b04ebda99d1fdaeade
Zhafranabimata <zhafranabimata@gmail.com>
2026-09-25 09:38:50 +0700

    Fixlastry

--- src/main/java/id/ac/polinema/oop/Customer.java
+++ src/main/java/id/ac/polinema/oop/Customer.java
@@ -42,6 +42,8 @@ public class Customer {
     public void setName(String name) {
         if (name != " " || name != null){
             this.name = name;
+        } else{
+            System.out.print("Namanya Kosong!")
         }
     }
 }
