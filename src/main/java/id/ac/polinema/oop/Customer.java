f2d8e4bfeee85998e47b3df2a6b4658c8f581c8c
Zhafranabimata <zhafranabimata@gmail.com>
2026-09-25 09:40:29 +0700

    FixError

--- src/main/java/id/ac/polinema/oop/Customer.java
+++ src/main/java/id/ac/polinema/oop/Customer.java
@@ -43,7 +43,7 @@ public class Customer {
         if (name != " " || name != null){
             this.name = name;
         } else{
-            System.out.print("Namanya Kosong!")
+            System.out.print("Namanya Kosong!");
         }
     }
 }
