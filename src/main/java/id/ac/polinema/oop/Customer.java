23e61f99978f6c9e2c45471b8d8e39eff2b24d8c
Zhafranabimata <zhafranabimata@gmail.com>
2026-09-25 09:31:53 +0700

    Fix new 2

--- src/main/java/id/ac/polinema/oop/Customer.java
+++ src/main/java/id/ac/polinema/oop/Customer.java
@@ -40,7 +40,7 @@ public class Customer {
      * @param name new name
      */
     public void setName(String name) {
-        if (name != " "){
+        if (name != " " || name == null){
             this.name = name;
         }
     }
